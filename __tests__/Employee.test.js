568c1b8a65005460fe63c89a4b98964cc3055a12
Irish Moore <jasonamoore@frontier.com>
2022-02-05 14:41:23 -0500

    employee test setup

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,3 +4,21 @@ test("Can create an new employee", () => {
     const employeeInstance = new Employee();
     expect(typeof(employeeInstance)).toBe("object");
 })
+
+test("Testing name.", () => {
+    const name = "Jason";
+    const employeeInstance = new Employee(name);
+    expect(employeeInstance.name).toBe(name);
+})
+
+test("Testing ID.", () => {
+    const id = 2;
+    const employeeInstance = new Employee("Jason", id)
+    expect(employeeInstance.id).toBe(id);
+})
+
+test("Testing email.", () => {
+    const email = "jasonamoore@frontier.com";
+    const employeeInstance = new Employee("Jason", 2, email);
+    expect(employeeInstance.email).toBe(email);
+})
