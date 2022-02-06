4ce8b7db7eb0e4e3b1708e6cba43fbccb32e5996
Irish Moore <jasonamoore@frontier.com>
2022-02-06 12:40:17 -0500

    built out intern role

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -11,3 +11,9 @@ test("Testing testSchool will return School Name", () => {
     const employeeInstance = new Intern("Jason", 2, "jasonamoore@frontier.com", testSchool);
     expect(employeeInstance.getSchool()).toBe(testSchool);
 });
+
+test("Testing role", () => {
+    const returnValue = "Intern";
+    const employeeInstance = new Intern("Jason", 2, "jasonamoore@frontier.com", "School Name");
+    expect(employeeInstance.getRole()).toBe(returnValue);
+});
