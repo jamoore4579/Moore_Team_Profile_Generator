14aefa9dfbf330209ca3492949f879484f48095d
Irish Moore <jasonamoore@frontier.com>
2022-02-06 12:16:52 -0500

    built out manager role

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -11,3 +11,9 @@ test("Testing officeNumber will return office number", () => {
     const employeeInstance = new Manager("Jason", 2, "jasonamoore@froniter.com", testOfficeNumber);
     expect(employeeInstance.getOfficeNumber()).toBe(testOfficeNumber);
 });
+
+test("Testing role", () => {
+    const returnValue = "Manager";
+    const employeeInstance = new Manager("Jason", 2, "jasonamoore@frontier.com", 43);
+    expect(employeeInstance.getRole()).toBe(returnValue);
+})
