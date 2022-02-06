92780c716b2f1a1b8feef7c8ae0ae5f7f5fa1bf2
Irish Moore <jasonamoore@frontier.com>
2022-02-05 22:14:37 -0500

    Finished Employee tests and buidling out class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -22,3 +22,27 @@ test("Testing email.", () => {
     const employeeInstance = new Employee("Jason", 2, email);
     expect(employeeInstance.email).toBe(email);
 })
+
+test("Gets name through getName method", () => {
+    const testName = "Jason";
+    const employeeInstance = new Employee(testName);
+    expect(employeeInstance.getName()).toBe(testName);
+})
+
+test("Can test ID through getID method", () => {
+    const testID = 2;
+    const employeeInstance = new Employee("Jason", testID);
+    expect(employeeInstance.getID()).toBe(testID);
+})
+
+test("Can test email through getEmail method", () => {
+    const testEmail = "jasonamoore@frontier.com";
+    const employeeInstance = new Employee("Jason", 2, testEmail);
+    expect(employeeInstance.getEmail()).toBe(testEmail);
+})
+
+test("Testing role", () => {
+    const returnValue ="Employee";
+    const employeeInstance = new Employee("Jason", 2, "jasonamoore@frontier.com");
+    expect(employeeInstance.getRole()).toBe(returnValue)
+})
