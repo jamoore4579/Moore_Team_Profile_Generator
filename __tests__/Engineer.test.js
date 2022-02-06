b75065d1ac629f118dc2acdc5b8d506b27b8f78f
Irish Moore <jasonamoore@frontier.com>
2022-02-05 21:25:43 -0500

    working engineer tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,14 @@
+const { hasUncaughtExceptionCaptureCallback } = require("process");
+const Engineer = require("../lib/Engineer");
+
+test("Can create a github", () => {
+    const testGithub = "jamoore4579";
+    const employeeInstance = new Engineer("Jason", 2, "jasonamoore@frontier.com", testGithub);
+    expect(employeeInstance.Github).toBe(testGithub);
+});
+
+test("Testing getGithub will return github", () => {
+    const testGithub = "jamoore4579";
+    const employeeInstance = new Engineer("Jason", 2, "jasonamoore@frontier.com", testGithub);
+    expect(employeeInstance.getGithub()).toBe(testGithub);
+})
