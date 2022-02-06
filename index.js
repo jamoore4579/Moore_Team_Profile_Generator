d35775172ab13d15263def3ce84f965444b38eba
Irish Moore <jasonamoore@frontier.com>
2022-02-06 15:14:57 -0500

    add inquirer code to index file

--- index.js
+++ index.js
@@ -61,4 +61,102 @@ const managerQuestions = () => {
     });
 };
 
+const engineerQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the Engineer\'s name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the Engineer\'s ID?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the Engineer\'s Email Address?',
+        },
+        {
+            type: 'list',
+            name: 'github',
+            message: 'What is the Engineer\'s GitHub username?',
+        },
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'What type of Team Member would you like to add next?',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
+        }
+    ])
+
+    .then((engineerAnswers) => {
+        const engineer = new Engineer(engineerAnswers.name, engineerAnswers.id, engineerAnswers.email, engineerAnswers.github)
+        team.push(engineer)
+        switch(engineerAnswers.addMember) {
+            case 'Engineer':
+                engineerQuestions();
+                break;
+            case 'Intern':
+                internQuestions();
+                break;
+            default:
+            writeToFile('dist/index.html', generateTeam(team))
+        }
+    })
+};
+
+const internQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the Intern\'s name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the Intern\'s ID?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the Intern\'s Email Address?',
+        },
+        {
+            type: 'list',
+            name: 'github',
+            message: 'What is the Intern\'s school?',
+        },
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'What type of Team Member would you like to add next?',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
+        }
+    ])
+
+    .then((internAnswers) => {
+        const intern = new Intern(internAnswers.name, internAnswers.id, internAnswers.email, internAnswers.school)
+        team.push(intern)
+        switch(internAnswers.addMember){
+            case 'Engineer':
+                engineerQuestions();
+                break;
+            case 'Intern':
+                internQuestions();
+                break;
+            default:
+                writeToFile('dist/index.html', generateTeam(team))
+        }
+    })
+}
 managerQuestions();
+
+function writeToFile(filename, data) {
+    fs.writeFile(filename, data, (err) => {
+        if(err) throw err;
+        console.log('file saved')
+    });
+};
